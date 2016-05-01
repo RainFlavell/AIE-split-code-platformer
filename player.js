42b2dba74c8ebb02b33c9bcada2857a083004272
RainFlavell <rain.flavell@gmail.com>
2016-05-01 11:48:20 +1000

    finished movement animations

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-
+var PLAYER_SPEED = 300;
 var LEFT = 0;
 var RIGHT = 1;
 var ANIM_IDLE_LEFT = 0;
@@ -48,30 +48,58 @@ Player.prototype.draw = function()
 {
   this.sprite.draw(context, this.position.x, this.position.y);
     // hang on, where did this variable come from!
-    context.save();
-    context.translate(this.position.x, this.position.y); context.rotate(this.rotation);
-    //context.drawImage(this.image, -this.width / 2, -this.height / 2);
-    context.drawSprite(this.sprite,0,0);
-    context.restore();
+
 }
-console.log(this.position);
+
 
 Player.prototype.update = function(deltaTime)
 {
   this.sprite.update(deltaTime);
-    //var velocity.set(0,0);
-    var left = false;
-    var right = false;
-    var jump = false;
-    // check keypress events
-    if(keyboard.isKeyDown(keyboard.KEY_LEFT) == true) {
-      left = true;
+  //var velocity.set(0,0);
+  var left = false;
+  var right = false;
+  var jump = false;
+
+  // check keypress events
+  if(keyboard.isKeyDown(keyboard.KEY_LEFT) == true) {
+    left = true;
+    this.direction = LEFT;
+    if(this.sprite.currentAnimation != ANIM_WALK_LEFT &&
+    this.jumping == false)
+    this.sprite.setAnimation(ANIM_WALK_LEFT);
+  }
+
+  else  if(keyboard.isKeyDown(keyboard.KEY_RIGHT) == true) {
+    right = true;
+    this.direction = RIGHT;
+    if(this.sprite.currentAnimation != ANIM_WALK_RIGHT &&
+    this.jumping == false)
+    this.sprite.setAnimation(ANIM_WALK_RIGHT);
+  }
+  else {
+    if(this.jumping == false && this.falling == false)
+    {
+      if(this.direction == LEFT)
+      {
+        if(this.sprite.currentAnimation != ANIM_IDLE_LEFT)
+        this.sprite.setAnimation(ANIM_IDLE_LEFT);
+      }
+      else
+      {
+        if(this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
+        this.sprite.setAnimation(ANIM_IDLE_RIGHT);
+      }
     }
-    if(keyboard.isKeyDown(keyboard.KEY_RIGHT) == true) {
-      right = true;
-    }
+  }
+
     if(keyboard.isKeyDown(keyboard.KEY_SPACE) == true) {
       jump = true;
+      if(left == true){
+        this.sprite.setAnimation(ANIM_JUMP_LEFT);
+      }
+      if(right == true){
+        this.sprite.setAnimation(ANIM_JUMP_RIGHT);
+      }
     }
 
     var wasleft = this.velocity.x < 0;
@@ -92,6 +120,10 @@ Player.prototype.update = function(deltaTime)
     {
       ddy = ddy - JUMP; // apply an instantaneous (large) vertical impulse
       this.jumping = true;
+      if(this.direction == LEFT)
+      this.sprite.setAnimation(ANIM_JUMP_LEFT)
+      else
+        this.sprite.setAnimation(ANIM_JUMP_RIGHT)
     }
 
     // calculate the new position and velocity:
