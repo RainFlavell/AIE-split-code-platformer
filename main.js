6288172754cc759d760d462098267c2aa8123a64
RainFlavell <rain.flavell@gmail.com>
2016-05-11 21:58:35 +1000

    scrolling is smooth
    now ad sound

--- main.js
+++ main.js
@@ -134,41 +134,71 @@ function cellAtTileCoord(layer, tx, ty)
   return 0;
   return cells[layer][ty][tx];
 };
+Player.prototype.draw = function()
+{
+  this.sprite.draw(context,
+    this.position.x - worldOffsetX,
+    this.position.y);
+  }
 
-function tileToPixel(tile)
-{
-  return tile * TILE;
-};
-function pixelToTile(pixel)
-{
-  return Math.floor(pixel/TILE);
-};
-function bound(value, min, max)
-{
-  if(value < min)
-  return min;
-  if(value > max)
-  return max;
-  return value;
-}
+  function tileToPixel(tile)
+  {
+    return tile * TILE;
+  };
+  function pixelToTile(pixel)
+  {
+    return Math.floor(pixel/TILE);
+  };
+  function bound(value, min, max)
+  {
+    if(value < min)
+    return min;
+    if(value > max)
+    return max;
+    return value;
+  }
 
 function drawMap() {
-    for (var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) {
-        var idx = 0;
-        for (var y = 0; y < level1.layers[layerIdx].height; y++) {
-            for (var x = 0; x < level1.layers[layerIdx].width; x++) {
-                if (level1.layers[layerIdx].data[idx] != 0) {
-                    // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
-                    // correct tile
-                    var tileIndex = level1.layers[layerIdx].data[idx] - 1;
-                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
-                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
-                    context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x * TILE, (y - 1) * TILE, TILESET_TILE, TILESET_TILE);
-                }
-                idx++;
-            }
+  var maxTiles = Math.floor(SCREEN_WIDTH / TILE) +2;
+  var tileX = pixelToTile(player.position.x);
+  var offsetX = TILE + Math.floor(player.position.x%TILE);
+  startX = tileX - Math.floor(maxTiles / 2);
+  if (startX < -1)
+  {
+    startX = 0;
+    offsetX = 0;
+  }
+if(startX > MAP.tw - maxTiles)
+{
+  startX = MAP.tw - maxTiles + 1;
+  offsetX = TILE;
+}
+worldOffsetX = startX * TILE + offsetX;
+
+for( var layerIdx=0; layerIdx < LAYER_COUNT; layerIdx++ )
+{
+  for( var y = 0; y < level1.layers[layerIdx].height; y++ )
+  {
+    var idx = y * level1.layers[layerIdx].width + startX;
+    for( var x = startX; x < startX + maxTiles; x++ )
+    {
+      if( level1.layers[layerIdx].data[idx] != 0 )
+      {
+        // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile),
+        // so subtract one from the tileset id to get the
+        // correct tile
+        var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+        var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) *
+        (TILESET_TILE + TILESET_SPACING);
+        var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) *
+        (TILESET_TILE + TILESET_SPACING);
+        context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE,
+          (x-startX)*TILE - offsetX, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
         }
+        idx++;
+      }
     }
+  }
 }
 
 
@@ -177,10 +207,9 @@ function drawMap() {
 function run() {
     context.fillStyle = "#ccc";
     context.fillRect(0, 0, canvas.width, canvas.height);
-    drawMap()
     var deltaTime = getDeltaTime();
-
     player.update(deltaTime);
+    drawMap()
     player.draw();
 
     //context.drawImage(chuckNorris, SCREEN_WIDTH/2 - chuckNorris.width/2, SCREEN_HEIGHT/2 - chuckNorris.height/2);
